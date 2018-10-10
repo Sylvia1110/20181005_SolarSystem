07e420548cc485de6f457f21ef980983f88f5f7c
韓舒容 <hanshurong@hanshurongde-MacBook-Pro.local>
2018-10-10 23:16:21 +0800

    1010 first commit

---
+++ Assets/script/earth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public float y;
+    // Update is called once per frame
+    void Update()
+    {
+
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, y * Time.deltaTime, 0);
+        transform.Rotate(angle);
+
+    }
+}
